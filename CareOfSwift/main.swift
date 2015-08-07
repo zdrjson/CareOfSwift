2b5a241a777d2898cb35f335472bb3ecc52ccc61
张德荣 <zdrjson@sina.com>
2015-08-07 23:38:31 +0800

    join in CareOfSwift

---
+++ CareOfSwift/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  CareOfSwift
+//
+//  Created by 张德荣 on 15/8/7.
+//  Copyright (c) 2015年 JsonZhang. All rights reserved.
+//
+
+import Foundation
+
+
+
+println("\(9%4)")
+println("\(9%(-4))")
+println("\(8%2.5)")
